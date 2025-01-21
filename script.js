d9c5b19a78d28014a50deb70864dafc7cf146a39
Josetovx <10379711+Josetovx@users.noreply.github.com>
2025-01-21 12:23:27 +0100

    Write computer choice function
    Write a function with the choice of te computer.
    It can be rock, paper or scissors.

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("Hello Odin World!")
+
+function getComputerChoice() {
+    let result=Math.random().toPrecision(2);
+    if (result>=0 && result<0.34) {
+        console.log("rock")
+    } else if (result>=0.34 && result<0.67) {
+        console.log("paper")
+    } else {
+        console.log("scissors")
+    }
+    return result
+}
