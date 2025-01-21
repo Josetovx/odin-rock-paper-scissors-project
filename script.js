c091985004aa2dab8f3cb50baae74f7d32ae7aad
Josetovx <10379711+Josetovx@users.noreply.github.com>
2025-01-21 13:28:25 +0100

    Begin playRound function and correct
    Start writing the playRound function, for a single round game.
    Also, make corrections in getComputerChoice function,
    writing "computerChoice" instead "result".

--- script.js
+++ script.js
@@ -4,15 +4,15 @@ console.log("Hello Odin World!")
 Computer choice will have the same probability to 
 get whatever of the three options */
 function getComputerChoice() {
-    let result=Math.random().toPrecision(2);
-    if (result>=0 && result<0.34) {
+    let computerChoice=Math.random().toPrecision(2);
+    if (computerChoice>=0 && computerChoice<0.34) {
         console.log("rock")
-    } else if (result>=0.34 && result<0.67) {
+    } else if (computerChoice>=0.34 && computerChoice<0.67) {
         console.log("paper")
     } else {
         console.log("scissors")
     }
-    return result
+    return computerChoice
 }
 
 /* Then, create a function to get the human choice.
@@ -37,3 +37,9 @@ function getHumanChoice() {
 in order to keep track of the players score */
 let humanScrore=0
 let computerScore=0
+
+/* Create a function to play a single round and
+increment the round winner's score */
+funcion playRound(humanChoice, computerChoice) {
+    
+}
